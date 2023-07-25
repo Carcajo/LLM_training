b4751bb27c59e02616cb5b7873b860a915701ef9
Maxim Puhov <93794796+Carcajo@users.noreply.github.com>
2023-07-25 23:23:15 +0300

    Update LaMDA.py

--- LaMDA.py
+++ LaMDA.py
@@ -1,64 +1,15 @@
-# import tensorflow as tf
-# import tensorflow_hub as hub
-#
-# # Загружаем модель LaMDA
-# model = hub.load("https://tfhub.dev/google/language/LaMDA-1.0-uncased-v1")
-#
-# # Читаем данные из файла
-# with open("data.txt", "r") as f:
-#   data = f.read().splitlines()
-#
-# # Разделяем данные на слова и синонимы
-# words = []
-# synonyms = []
-# for line in data:
-#   word, synonym = line.split(" ")
-#   words.append(word)
-#   synonyms.append(synonym)
-#
-# # Создаем словарь
-# vocabulary = set(words + synonyms)
-#
-# # Кодируем данные
-# encoder = tf.keras.layers.TextVectorization(vocabulary=vocabulary)
-# encoded_words = encoder(words)
-# encoded_synonyms = encoder(synonyms)
-#
-# # Создаем модель
-# model = tf.keras.models.Sequential([
-#   encoder,
-#   tf.keras.layers.Dense(128, activation="relu"),
-#   tf.keras.layers.Dense(64, activation="relu"),
-#   tf.keras.layers.Dense(len(vocabulary), activation="softmax")
-# ])
-#
-# # Компилируем модель
-# model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
-#
-# # Обучаем модель
-# model.fit(encoded_words, encoded_synonyms, epochs=10)
-#
-# # Оцениваем модель
-# model.evaluate(encoded_words, encoded_synonyms)
-#
-# # Сохраняем модель
-# model.save("model.h5")
 import re
 import tensorflow_datasets as tfds
 import tensorflow as tf
 import tensorflow_hub as hub
 from keras.layers import Input, Lambda, Dense
 
-# Загрузка модели Claude
 model = hub.load("https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/4")
-
 
 def preprocess(text):
   text = text.lower()
   text = re.sub(r"[^а-я ]", "", text)
   return text
-
-
 
 words = []
 synonyms = []
@@ -69,9 +20,7 @@ with open("data.txt") as f:
     words.append(preprocess(word))
     synonyms.append(preprocess(synonym))
 
-
 tokenizer = tfds.features.text.SubwordTextEncoder.build_from_corpus(words + synonyms, target_vocab_size=2 ** 15)
-
 
 encoded_words = tokenizer.encode(words)
 encoded_synonyms = tokenizer.encode(synonyms)
